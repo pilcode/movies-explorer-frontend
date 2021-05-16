de3685d01fc8bfeff1b85df4d8ebd8c10f6997a4
pilcode <redquinredquin@yandex.ru>
2021-05-16 08:28:13 +0200

    Feat: burger-menu

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,55 @@
 import './App.css';
+import React from 'react';
+// import { Route, Switch } from 'react-router-dom';
+// import {CurrentUserContext} from '../contexts/CurrentUserContext';
+// import ProtectedRoute from './ProtectedRoute';
+import Header from '../Header/Header';
+
+
 
 function App() {
+  const [isBurgerMenuOpen, setIsBurgerMenuOpen] = React.useState(false);
+
+  function handleBurgerMenu() {
+    console.log('да сработало в апп открытие хендлер')
+    setIsBurgerMenuOpen(true);
+  }
+
+  function closeBurgerMenu () {
+    console.log('да сработало в апп при закрытии')
+
+    setIsBurgerMenuOpen(false);
+  }
+
   return (
-    <div className="app">
-      <p>Привет, Мир!</p>
-    </div>
+    // <CurrentUserContext.Provider value={currentUser}>
+      <div className="app">
+        {/* <p>Привет, Мир!</p> */}
+        <Header
+          isOpen={isBurgerMenuOpen}
+          onClose={closeBurgerMenu}
+          onBurgerMenu={handleBurgerMenu}
+        />
+        {/* <Switch>
+          <ProtectedRoute
+            exact path="/"
+            // component={Main}
+          />
+          <Route path="/signin">
+            <Login
+              onLogin={hsndleLogin}
+            />
+          </Route>
+          <Route path="/signup">
+            <Register
+              onRegister={handleRegister}
+            />
+          </Route>
+        </Switch>  */}
+      </div>
+      /* <EditProfilePopup></EditProfilePopup> */
+
+    // </CurrentUserContext.Provider>
   );
 }
 
---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,59 @@
+// import './Navigation.css'
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Navigation() {
+  return (
+
+    <nav className="navigation">
+      <ul className="navigation__list">
+          <li className="navigation__item">
+            <Link className="navigation__link" to='/movies'>Фильмы</Link>
+          </li>
+          <li className="navigation__item">
+            <Link className="navigation__link" to='/saved-movies'>Сохраненные фильмы</Link>
+          </li>
+          <li className="navigation__item">
+            <Link className="navigation__link navigation__link_type_oval" to='/profile'>Аккаунт</Link>
+          </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navigation;
+
+// const menus = {
+//   startMenu: [
+//     { name: 'Фильмы', link: '/movies' },
+//     { name: 'Сохраненные фильмы', link: '/saved-movies' },
+//     { name: 'Аккаунт', link: '/profile', classMod: "navigation__link_type_oval" },
+//   ],
+  // burgerMenu: [
+  //   { name: 'Главная', link: '/' },
+  //   { name: 'Фильмы', link: '/movies' },
+  //   { name: 'Сохраненные фильмы', link: '/saved-movies' },
+  //   { name: 'Аккаунт', link: '/profile', classMod: "navigation__link_type_oval" },
+  // ]
+// }
+
+// const startMenu = [
+//   { name: 'Фильмы', link: '/movies', classMod: ''},
+//   { name: 'Сохраненные фильмы', link: '/saved-movies', classMod: ''},
+//   { name: 'Аккаунт', link: '/profile', classMod: "navigation__link_type_oval" },
+// ]
+// function Navigation() {
+//   return (
+
+    // <nav className="navigation">
+    //   <ul className="navigation__list">
+    //     { startMenu.map((item) => (
+    //       <li className="navigation__item" key={index}>
+    //         <Link className={"navigation__link" + (!!item.classMod ? ' ' + item.classMod : '')  } to={item.link}>{item.name}</Link>
+    //       </li>
+    //     )) }
+    //   </ul>
+    // </nav>
+
+//   );
+// }
